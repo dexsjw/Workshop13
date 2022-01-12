8ca8b394c59dcbf6199868a26b6a45a83bad3adb
Dexter Soh <dextersjw@gmail.com>
2022-01-13 00:31:48 +0800

    add test case

--- src/test/java/nus/iss/workshop13/Workshop13ApplicationTests.java
+++ src/test/java/nus/iss/workshop13/Workshop13ApplicationTests.java
@@ -3,11 +3,25 @@ package nus.iss.workshop13;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import nus.iss.workshop13.model.Contact;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class Workshop13ApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void testContact() throws Exception {
+		Contact c = new Contact();
+		c.setName("Kenneth");
+		c.setEmail("a@a.com");
+		c.setPhoneNum("1234567");
+		// assert equals to the setter value
+        	assertEquals(c.getEmail(), "a@a.com");
 	}
 
 }
